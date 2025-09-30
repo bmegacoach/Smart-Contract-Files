efb1f657884a493117f37c7e5eaeda4cb8354286
Bmegacoach <bmegacoach1@gmail.com>
2025-09-30 20:46:21 +0500

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,14 +1,36 @@
 
 
-contract HashForEther {
 
-    function withdrawWinnings() public payable {
-        // Winner if the last 8 hex characters of the address are 0.
-        if (uint32(msg.sender) == 0)
-            _sendWinnings();
-    }
+contract Caller {
 
-    function _sendWinnings() public {
-        msg.sender.transfer(address(this).balance);
-    }
+	address public fixed_address;
+	address public stored_address;
+
+	uint256 statevar;
+
+	constructor(address addr) public {
+		fixed_address = addr;
+	}
+
+	function thisisfine() public {
+	    fixed_address.call("");
+	}
+
+	function reentrancy() public {
+	    fixed_address.call("");
+	    statevar = 0;
+	}
+
+	function calluseraddress(address addr) public {
+	    addr.call("");
+	}
+
+	function callstoredaddress() public {
+	    stored_address.call("");
+	}
+
+	function setstoredaddress(address addr) public {
+	    stored_address = addr;
+	}
+
 }
